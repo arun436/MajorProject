ba87fb3f69e3c93b56f595c9c5c32a0c9a26d73c
arun436 <arunkumarchintha136@gmail.com>
2021-02-20 00:53:31 +0530

    added component and styles folder

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>I will do my Major Project Work here</h1>
+    <h1>I am Arun Kumar Chintha</h1>
+    </>
+  );
+}
+
+export default App;
